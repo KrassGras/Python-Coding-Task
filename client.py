1cd1739161f9a1a72a798132b2234f1b58544f8c
Carsten M <145705593+KrassGras@users.noreply.github.com>
2025-05-01 21:45:43 +0200

    added Docstrings to the functions

--- client.py
+++ client.py
@@ -21,6 +21,11 @@ def parse_args():
 
 
 def send_csv_to_server(path: str) -> list[dict]:
+    """
+    Sends a POST-Message to the Server-API and returns the enriched data as a list of dictionaries
+    args: path (str) the path to the csv_file you want to send
+    returns: list of dictionaries
+    """
     url = "http://localhost:8000/upload.csv/"
     with open(args.csv_file, "rb") as f:
         files = {"file": (path, f, "text/csv")}
@@ -28,8 +33,13 @@ def send_csv_to_server(path: str) -> list[dict]:
 
         return response.json()
 
-
 def calculate_hu_age(hu: str) -> int:
+   """
+   Expects the date of the hu (iso formatted), calculates the age (in months) of the hu with the current date and
+   returns it
+   args: hu (str)
+   returns age in months (int)
+   """
    hu_datelist =  hu.split("-")
    hu_date = datetime(int(hu_datelist[0]), int(hu_datelist[1]), int(hu_datelist[2]))
    today = datetime.now()
@@ -40,17 +50,21 @@ def calculate_hu_age(hu: str) -> int:
 
 
 def create_excel(additional_columns: list, tinted: bool, csv_data: list[dict]):
+    """
+    Expects the additional columns which are given in the command by -k, a tinted value (also given in the
+    command (-c), csv_data as a list of dictionaies and creates an excel-file of the csv_data
+    args: additional_columns (list), tinted (bool), csv_data (list of dictionaries)
+    """
     wb = Workbook()
     ws = wb.active
     ws.title = "vehicles"
     column_names = ["rnr"]
-    row_count = 0
-    color_list = list()
 
     if additional_columns:
         column_names.extend(additional_columns)
     ws.append(column_names)
 
+    # color codes for the hu
     red_fill = PatternFill(start_color="FFb30000", fill_type="solid")
     orange_fill = PatternFill(start_color="FFFFA500", fill_type="solid")
     green_fill = PatternFill(start_color="FF007500", fill_type="solid")
@@ -58,7 +72,6 @@ def create_excel(additional_columns: list, tinted: bool, csv_data: list[dict]):
     for idx, row in enumerate(csv_data, start=2):
         row_data = [row.get("rnr")] + [row.get(col) for col in additional_columns]
         ws.append(row_data)
-
 
         if tinted and "hu" in row:
             age_in_months = calculate_hu_age(row["hu"])
@@ -95,4 +108,3 @@ if __name__ == "__main__":
     test = json.dumps(sorted_data)
 
     create_excel(list(args.keys), args.colored, sorted_data)
-    print(args.keys, args.colored)
